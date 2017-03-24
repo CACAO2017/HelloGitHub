081e75cb4c92438bca0899be2150e5bcf4fdc028
Ferin <kevin.ferin.3@gmail.com>
2017-03-24 16:39:56 +0100

    Groupe

---
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/Groupe6.java
@@ -0,0 +1,5 @@
+package fr.imta.prime.cacao2017.hellogithub;
+
+public class Groupe6 {
+
+}
--- HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/HelloGitHub.java
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/HelloGitHub.java
@@ -3,7 +3,7 @@ package fr.imta.prime.cacao2017.hellogithub;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-
+		
 	}
 
 }
