9df3cc3712e3f1684453f8f364f6de9c80854dc8
imagne16 <inesmagnet@gmail.com>
2017-03-24 16:59:53 +0100

    ajout des noms des identifiants imagne16 et zmariette16

--- HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/Helloworld.java
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/Helloworld.java
@@ -3,6 +3,8 @@ package fr.imta.prime.cacao2017.hellogithub;
 public class Helloworld {
 	public static void afficher(){
 		System.out.println("danjud");
+		System.out.println("imagne16");
+		System.out.println("zmariette16");
 	}
 	
 	public static void main(String[] args) {
