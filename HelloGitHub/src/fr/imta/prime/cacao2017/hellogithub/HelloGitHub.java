c7662788607a036dc2f17506f10659d004910f92
AnaisBel <anbellance@gmail.com>
2017-03-24 17:14:18 +0100

    final

--- HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/HelloGitHub.java
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/HelloGitHub.java
@@ -1,14 +1,14 @@
 package fr.imta.prime.cacao2017.hellogithub;
 
 public class HelloGitHub {
-	public String name(){
-		System.out.println("AnaisBel");
-		System.out.println("adrienpin");
-	}
+		
+
 
 	public static void main(String[] args) {
 		System.out.println("Lolotteisyoung");
 		System.out.println("swerne16");
+		System.out.println("AnaisBel");
+		System.out.println("adrienpin");
 	}		
 	
 
