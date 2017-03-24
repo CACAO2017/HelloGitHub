3efda2183ccd5d272fdac3685d3884417db35517
AnaisBel <anbellance@gmail.com>
2017-03-24 16:53:01 +0100

    Change

--- HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/HelloGitHub.java
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/HelloGitHub.java
@@ -1,9 +1,13 @@
 package fr.imta.prime.cacao2017.hellogithub;
 
 public class HelloGitHub {
+	public String Name(String a){
+		return a;
+	}
 
 	public static void main(String[] args) {
-
-	}
+		String a="";
+		System.out.println(a);
+	}		
 
 }
