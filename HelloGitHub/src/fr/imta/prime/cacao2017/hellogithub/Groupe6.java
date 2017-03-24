21c6b1e97da2ccf41e989676b1c9d224b0cc8837
antoineroson <antoine.roson@gmail.com>
2017-03-24 16:48:43 +0100

    Antoine

--- HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/Groupe6.java
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/Groupe6.java
@@ -3,6 +3,7 @@ package fr.imta.prime.cacao2017.hellogithub;
 public class Groupe6 {
 	public static void main (String[] args) {
 		System.out.println("Kevin");
+		System.out.println("Antoine");
 	}
 	
 }
