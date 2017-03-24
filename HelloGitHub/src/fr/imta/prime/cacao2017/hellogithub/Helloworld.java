da3b606afdb2ac3a63a84de8e6fffe56477f30b1
Alexandra Micaud <alexandra.micaud@orange.fr>
2017-03-24 16:54:14 +0100

    ajout du amicau16

--- HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/Helloworld.java
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/Helloworld.java
@@ -3,6 +3,7 @@ package fr.imta.prime.cacao2017.hellogithub;
 public class Helloworld {
 	public static void afficher(){
 		System.out.println("danjud");
+		System.out.println("amicau16");
 	}
 	
 	public static void main(String[] args) {
