b7b2bcc3da615254bfd812afec94382e70f6616a
Ferin <kevin.ferin.3@gmail.com>
2017-03-24 16:52:17 +0100

    kev

--- HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/Groupe6.java
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/Groupe6.java
@@ -1,9 +1,13 @@
 package fr.imta.prime.cacao2017.hellogithub;
 
 public class Groupe6 {
+	
+	public static void Nom ( String nom) {
+		System.out.println(nom);
+	}
 	public static void main (String[] args) {
-		System.out.println("Kevin");
-		System.out.println("Antoine");
+		Nom("War20");
+		Nom("Antoine");
 	}
 	
 }
