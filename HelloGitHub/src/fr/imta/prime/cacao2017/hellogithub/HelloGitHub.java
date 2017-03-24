4cbcfbd0ba63cdd41268f59ddea0be1003f6cb33
AnaisBel <anbellance@gmail.com>
2017-03-24 16:55:47 +0100

    v1

--- HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/HelloGitHub.java
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/HelloGitHub.java
@@ -1,13 +1,12 @@
 package fr.imta.prime.cacao2017.hellogithub;
 
 public class HelloGitHub {
-	public String Name(String a){
-		return a;
+	public String name(){
+		System.out.println("AnaisBel");
 	}
 
 	public static void main(String[] args) {
-		String a="";
-		System.out.println(a);
+		
 	}		
 
 }
