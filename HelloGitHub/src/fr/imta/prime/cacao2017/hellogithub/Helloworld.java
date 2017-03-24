80132703917d6a1f113c5fcbb131c5d1642473f7
danjud <dan.judkiewicz@gmail.com>
2017-03-24 16:49:27 +0100

    classe ajoutée

---
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/Helloworld.java
@@ -0,0 +1,11 @@
+package fr.imta.prime.cacao2017.hellogithub;
+
+public class Helloworld {
+	public static void afficher(){
+		System.out.println("danjud");
+	}
+	
+	public static void main(String[] args) {
+		afficher();
+	}
+}
