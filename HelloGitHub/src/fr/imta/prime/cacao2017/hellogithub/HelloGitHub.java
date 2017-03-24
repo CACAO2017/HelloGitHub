754098902c6cb9318cb517c78ebd7b0489211db2
adripincemin <adrien.pincemin@gmail.com>
2017-03-24 17:05:10 +0100

    v3

--- HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/HelloGitHub.java
+++ HelloGitHub/src/fr/imta/prime/cacao2017/hellogithub/HelloGitHub.java
@@ -3,6 +3,7 @@ package fr.imta.prime.cacao2017.hellogithub;
 public class HelloGitHub {
 	public String name(){
 		System.out.println("AnaisBel");
+		System.out.println("adrienpin");
 	}
 
 	public static void main(String[] args) {
